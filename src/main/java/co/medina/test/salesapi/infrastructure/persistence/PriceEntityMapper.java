fcc2bcaaf56de59d269af9b424d8d0368475799f
Camilo Medina <medina1camilo@gmail.com>
2026-09-06 01:10:50 +0200

    [main] add price mapper.

---
+++ src/main/java/co/medina/test/salesapi/infrastructure/persistence/PriceEntityMapper.java
@@ -0,0 +1,21 @@
+package co.medina.test.salesapi.infrastructure.persistence;
+
+import co.medina.test.salesapi.domain.model.Money;
+import co.medina.test.salesapi.domain.model.Price;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+@Mapper(componentModel = "spring")
+public interface PriceEntityMapper {
+
+	@Mapping(target = "price", expression = "java(toMoney(entity.getPrice(), entity.getCurrency()))")
+	Price toDomain(PriceEntity entity);
+
+	default Money toMoney(BigDecimal amount, String currencyCode) {
+		return new Money(amount, Currency.getInstance(currencyCode));
+	}
+
+}
