75d0886ad41b103333296982742ee9309d7f0ad6
Camilo Medina <medina1camilo@gmail.com>
2026-09-06 01:36:53 +0200

    [main] add api first approach.

--- src/main/java/co/medina/test/salesapi/SalesApiApplication.java
+++ src/main/java/co/medina/test/salesapi/SalesApiApplication.java
@@ -1,9 +1,16 @@
 package co.medina.test.salesapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Sales API",
+        version = "v1",
+        description = "Looks up the single applicable price rate for a brand, product, and application date."
+))
 public class SalesApiApplication {
 
     static void main(String[] args) {
